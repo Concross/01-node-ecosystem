34aebbfea30d46cbedffc667c5e5753a5bfc7d58
Concross <cacrossley22@gmail.com>
2018-09-06 08:37:43 -0700

    refactor tests for best practices

--- lab-connor/test/arithmetic.test.js
+++ lab-connor/test/arithmetic.test.js
@@ -2,16 +2,39 @@
 
 const arithmetic = require('../lib/arithmetic');
 
-test('Should be called with numbers', () => {
-  let actual = [arithmetic.add(2, 2), arithmetic.sub(6, 2)];
+test('Should return 4 when called with 2 and 2', () => {
+  let actual = arithmetic.add(2, 2);
   let expected = 4;
 
-  actual.forEach(func => expect(func).toBe(expected));
+  expect(actual).toBe(expected);
+});
+
+test('Should return 4 when called with 6 and 2', () => {
+  let actual = arithmetic.sub(6, 2);
+  let expected = 4;
+
+  expect(actual).toBe(expected);
 });
 
 test('Should return null if any args are NaN', () => {
-  let expected = null;
-  let actual = [arithmetic.add(true, 2), arithmetic.sub(2, true)];
+  let add = arithmetic.add;
 
-  actual.forEach(func => expect(func).toBe(expected));
+  expect(add([], 2)).toBeNull();
+  expect(add(true, 2)).toBeNull();
+  expect(add('4', 2)).toBeNull();
+  expect(add(null, 2)).toBeNull();
+  expect(add(undefined, 2)).toBeNull();
+  expect(add('2', '2')).toBeNull();
+
 });
+
+test('Should return null if any args are NaN', () => {
+  let sub = arithmetic.sub;
+
+  expect(sub([], 2)).toBeNull();
+  expect(sub(true, 2)).toBeNull();
+  expect(sub('4', 2)).toBeNull();
+  expect(sub(null, 2)).toBeNull();
+  expect(sub(undefined, 2)).toBeNull();
+  expect(sub('2', '2')).toBeNull();
+})
